5e235ad3379393d54824b434085564e087a57601
Wesley Faircloth <wesleydfaircloth@gmail.com>
2020-06-23 16:48:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+// dwarfRollCall accepts an array of dwarves then numbers the dwarves in a line 1. ??? 2. ??? and returns it
 function dwarfRollCall(dwarves){
-
 var str = ''
   for(let i = 0; i < dwarves.length;i++){
     str += `${i+1}. ${dwarves[i]} `
   }
   return str
 }
-
+// summonCaptainPlanet accepts an array of calls and makes each element in the array all caps and adds a '!' to the end of the element then returns that new array
 function summonCaptainPlanet(planeteerCalls){
   var array = []
   for(let i = 0; i < planeteerCalls.length; i++){
@@ -14,10 +14,10 @@ function summonCaptainPlanet(planeteerCalls){
   }
   return array
 }
-
-function longPlaneteerCalls(assortedWords){
-  for(let i = 0; i < assortedWords.length; i++){
-    if(assortedWords[i].length > 4){
+//
+function longPlaneteerCalls(words){
+  for(let i = 0; i < words.length; i++){
+    if(words[i].length > 4){
       return true
     }
   }
